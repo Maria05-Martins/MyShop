00e4c2c97d0661501b6b4b2b805041b867f7496c
Maria05-Martins <maria05_martins@yahoo.com>
2020-07-23 16:04:48 +0530

    SQLRepository added

---
+++ MyShop/MyShop.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,62 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        internal DataContext context;
+        internal DbSet<T> dbSet;
+
+        public SQLRepository(DataContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<T>();
+        }
+        public IQueryable<T> Collection()
+        {
+            return dbSet;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            var t = dbSet.Find(id);
+            if (context.Entry(t).State == EntityState.Detached)
+                dbSet.Attach(t);
+
+            dbSet.Remove(t);
+        }
+
+        public IEnumerable<ProductCategory> Find(Func<object, bool> p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Find(string id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(T t)
+        {
+            dbSet.Add(t);
+        }
+
+        public void Update(T t, string id)
+        {
+            dbSet.Attach(t);
+            context.Entry(t).State = EntityState.Modified; 
+        }
+    }
+}
